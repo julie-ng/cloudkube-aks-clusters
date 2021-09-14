6de945c2cb53ebcc087bc7763de2ab32833429bd
Julie Ng <me@julie.io>
2021-09-14 17:39:20 +0200

    chore(deps): lock terraform provider versions

--- provider.tf
+++ provider.tf
@@ -1,5 +1,12 @@
 terraform {
   backend "azurerm" {} # see environments/backend.hcl.sample
+
+  required_providers {
+    azurerm = {
+      version = ">= 2.76.0"
+      source  = "hashicorp/azurerm"
+    }
+  }
 }
 
 provider "azurerm" {
